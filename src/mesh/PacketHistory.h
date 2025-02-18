5388692230522ba22485f83ba15939d34c9bdc62
Administrator <administrator@SITL-IT7948.local>
2025-02-18 09:39:33 +0530

    first commit

---
+++ src/mesh/PacketHistory.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Router.h"
+#include <unordered_set>
+
+/// We clear our old flood record 10 minutes after we see the last of it
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+#define FLOOD_EXPIRE_TIME (5 * 1000L) // Don't allow too many packets to accumulate when fuzzing.
+#else
+#define FLOOD_EXPIRE_TIME (10 * 60 * 1000L)
+#endif
+
+/**
+ * A record of a recent message broadcast
+ */
+struct PacketRecord {
+    NodeNum sender;
+    PacketId id;
+    uint32_t rxTimeMsec; // Unix time in msecs - the time we received it
+
+    bool operator==(const PacketRecord &p) const { return sender == p.sender && id == p.id; }
+};
+
+class PacketRecordHashFunction
+{
+  public:
+    size_t operator()(const PacketRecord &p) const { return (std::hash<NodeNum>()(p.sender)) ^ (std::hash<PacketId>()(p.id)); }
+};
+
+/**
+ * This is a mixin that adds a record of past packets we have seen
+ */
+class PacketHistory
+{
+  private:
+    std::unordered_set<PacketRecord, PacketRecordHashFunction> recentPackets;
+
+    void clearExpiredRecentPackets(); // clear all recentPackets older than FLOOD_EXPIRE_TIME
+
+  public:
+    PacketHistory();
+
+    /**
+     * Update recentBroadcasts and return true if we have already seen this packet
+     *
+     * @param withUpdate if true and not found we add an entry to recentPackets
+     */
+    bool wasSeenRecently(const meshtastic_MeshPacket *p, bool withUpdate = true);
+};
